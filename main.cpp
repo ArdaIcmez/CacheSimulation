3de4081ce5bd0e44c7e828b1a24d4d398e28a678
ArdaIcmez <aicmez@gmail.com>
2017-12-01 15:55:08 -0500

    Starting up the homework

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-hello world 
+#include <iostream>
+#include <cstdlib>
 
-eve
-
-dhara is here
-arda
-
+int main(int argc, char **argv) {
+  std::cout << "Hello World" << std::endl;
+  return EXIT_SUCCESS;
+}
