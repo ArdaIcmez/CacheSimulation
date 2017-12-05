ab2d611e886be800eb8a3873760901430a2111f3
ArdaIcmez <aicmez@gmail.com>
2017-12-05 00:04:40 -0500

    Commented out how the start of the simulation should be

--- simulation.cpp
+++ simulation.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 #include "simulation.h"
+
 void Simulation::analyseAdd(int type, int status) {
   //type is the type of instruction, status is the result of hit or miss
   switch (type) {
@@ -21,7 +25,21 @@ void Simulation::printResult() {
   // Print that result with numbers like the one in website, calculating stuff should be straightforward
 }
 
-void Simulation::start() {
-  // Go in a loop to read file, and call required functions
-  
+void Simulation::start(const std::string &filename) {
+// Go in a loop to read file, and call required functions
+  std::ifstream ifs(filename);
+  if (ifs.open()) {
+    std::string line;
+    while (ifs.good()) {
+      std::getline(ifs, line);
+      int type = line[0] - '0';
+      line = line.substr(2);  // get the hex address
+      //Call caches, L2 first then L1 if miss
+      //Get the result of checkHit of that cache
+      //Call analyseAdd(type,result);
+    }
+  } else {
+    std::cerr << "Couldn't open the test file!" << std::endl;
+    exit(EXIT_FAILURE);
+  }
 }
