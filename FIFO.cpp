83362a031514f9da4c8a39a67efb1bf5a35549cc
dp200 <dp200@duke.edu>
2017-12-06 21:56:41 -0500

    fifo again

---
+++ FIFO.cpp
@@ -0,0 +1,15 @@
+#include <queue>
+int main (void) {
+
+  std::map<size_t, std::queue<size_t> > fifoMap;
+  size_t index;
+  if(fifoMap[index].empty()) {
+    return 0; 
+  }
+  size_t way = fifoMap[index].front();
+  fifoMap[index].pop();
+  return way;
+
+  
+  return 0; 
+}
